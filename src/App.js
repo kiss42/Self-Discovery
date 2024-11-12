3e8feb9b3b361e6b2ffb431adc7ba440d6e54a26
Steven Pierre <96037516+kiss42@users.noreply.github.com>
2024-11-12 11:06:25 -0600

    Prepare for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import WorkbookContainer from './components/WorkbookContainer';
 import ShadowWorkbookContainer from './components/ShadowWorkbookContainer';
-import Chat from './components/Chat'; // Import the Chat component
+import Chat from './components/Chat';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter future={{ v7_relativeSplatPath: true }}>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/workbook" element={<WorkbookContainer />} />
           <Route path="/shadow-workbook" element={<ShadowWorkbookContainer />} />
-          <Route path="/chat" element={<Chat />} /> {/* Add the Chat route */}
+          <Route path="/chat" element={<Chat />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
